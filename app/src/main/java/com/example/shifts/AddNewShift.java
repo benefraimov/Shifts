1402eb295f9ccdbd635a0d5209c1b48d5419c5ce
בן אפריימוב <ben.a254@gmail.com>
2020-06-25 23:39:45 +0300

    new update

--- app/src/main/java/com/example/shifts/AddNewShift.java
+++ app/src/main/java/com/example/shifts/AddNewShift.java
@@ -1,14 +1,79 @@
 package com.example.shifts;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AddNewShift extends AppCompatActivity {
+
+    EditText startTime, endTime;
+    Button addShift;
+    FirebaseAuth mAuth;
+    DatabaseReference myRef;
+    String shift;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_shift);
+
+//        findViewById(R.id.btnViewShift).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(InsertShifts.this,ViewShifts.class));
+//            }
+//        });
+
+        mAuth = FirebaseAuth.getInstance();
+        myRef = FirebaseDatabase.getInstance().getReference("Users").child(mAuth.getUid()).child("Shifts");
+
+        startTime = findViewById(R.id.startTime);
+        endTime = findViewById(R.id.endTime);
+        addShift = findViewById(R.id.btnConfirm);
+
+
+
+        addShift.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                shift = "  08:00          " + endTime.getText().toString() + "          " +
+                        startTime.getText().toString() + "          1  ";
+                ValueEventListener valueEventListener = new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        List<String> list = new ArrayList<>();
+                        for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                            String eachShift = ds.getValue(String.class);
+                            list.add(eachShift);
+                        }
+                        list.add(shift);
+                        myRef.setValue(list);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                };
+                myRef.addListenerForSingleValueEvent(valueEventListener);
+                startActivity(new Intent(AddNewShift.this,WelcomePage.class));
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/shifts/WelcomePage.java
+++ app/src/main/java/com/example/shifts/WelcomePage.java
@@ -1,12 +1,18 @@
 package com.example.shifts;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -17,6 +23,8 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+
 public class WelcomePage extends AppCompatActivity {
 
     TextView userName;
@@ -25,6 +33,8 @@ public class WelcomePage extends AppCompatActivity {
     //Reference for Users
     DatabaseReference myRefUsers = FirebaseDatabase.getInstance().getReference("Users");
     FirebaseUser user;
+    ListView listView;
+    ArrayList<String> arrayList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,9 +42,53 @@ public class WelcomePage extends AppCompatActivity {
         setContentView(R.layout.activity_welcome_page);
 
         mAuth = FirebaseAuth.getInstance();
-        userName = findViewById(R.id.testNameApear);
-        user = mAuth.getCurrentUser();
+        //userName = findViewById(R.id.testNameApear);
+        //user = mAuth.getCurrentUser();
+        listView = (ListView)findViewById(R.id.listView);
 
+        myRefUsers = FirebaseDatabase.getInstance().getReference("Users").child(mAuth.getUid()).child("Shifts");
+
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,R.layout.each_item_row,
+                R.id.item,arrayList);
+
+        listView.setAdapter(arrayAdapter);
+
+        // Read from the database
+        ValueEventListener valueEventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot ds: snapshot.getChildren()){
+                    String value = ds.getValue(String.class);
+                    arrayList.add(value);
+                }
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        };
+        myRefUsers.addListenerForSingleValueEvent(valueEventListener);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
+                new AlertDialog.Builder(WelcomePage.this)
+                        .setTitle("Are you sure?")
+                        .setMessage("Do you want to delete this item?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                arrayList.remove(position);
+                                myRefUsers.setValue(arrayList);
+                                arrayAdapter.notifyDataSetChanged();
+                            }
+                        })
+                        .setNegativeButton("No", null)
+                        .show();
+            }
+        });
         //sharedPref = getSharedPreferences("shifts", Context.MODE_PRIVATE);
         //String name = sharedPref.getString("userName", "Not Found!");
 
@@ -42,20 +96,20 @@ public class WelcomePage extends AppCompatActivity {
         //userName.setText("Hello, " + name);
 
         // Read from the database
-        myRefUsers.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                User value = dataSnapshot.child(user.getUid()).getValue(User.class);
-                userName.setText("hello, " + value.getFullName());
-            }
-
-            @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-            }
-        });
+//        myRefUsers.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                // This method is called once with the initial value and again
+//                // whenever data at this location is updated.
+//                User value = dataSnapshot.child(user.getUid()).getValue(User.class);
+//                userName.setText("hello, " + value.getFullName());
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError error) {
+//                // Failed to read value
+//            }
+//        });
 
         (findViewById(R.id.btnMyWage)).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -76,8 +130,7 @@ public class WelcomePage extends AppCompatActivity {
         (findViewById(R.id.btnRefresh)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(WelcomePage.this,WelcomePage.class);
-                startActivity(i);
+                startActivity(new Intent(WelcomePage.this,WelcomePage.class));
             }
         });
 
